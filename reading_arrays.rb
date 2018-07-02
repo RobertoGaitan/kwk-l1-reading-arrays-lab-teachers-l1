06c1307f676e4dc07b243e6226bf381f4d4f4eec
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-02 13:21:49 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -35,7 +35,6 @@ end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  STUDENT_NAMES[0]
-  STUDENT_NAMES[1]
-  STUDENT_NAMES[2]
+  top_three = [STUDENT_NAMES[0], STUDENT_NAMES[1], STUDENT_NAMES[2]]
+  top_three
 end
